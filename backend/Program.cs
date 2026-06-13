56062723a6cbc4ccf99ce3e61fa6eacede147302
mikebat <mikebat88@gmail.com>
2026-06-13 12:57:04 +0200

    fixed wrong folder name

--- backend/Program.cs
+++ backend/Program.cs
@@ -5,7 +5,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using backend.Models;
-using backend.DBcontext;
+using backend.Data;
 using backend.DTOs;
 
 
