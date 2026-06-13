aaa09c2a45c3ce30da14617f7a347a14504a9178
mikebat <mikebat88@gmail.com>
2026-06-13 15:59:01 +0200

    fixed wrong proportions

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b4214ce22d346455f2239a588caca2817947cd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9863f26a6563d9bff8ddb8847ff87d4f530c8483")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
